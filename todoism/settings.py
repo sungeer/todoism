9bfc73089270e73aad0261724440a2ec7c44bae5
Want Sun <1013296326@qq.com>
2025-10-26 07:14:47 +0800

    add

--- todoism/settings.py
+++ todoism/settings.py
@@ -2,23 +2,17 @@ import os
 from datetime import datetime
 from pathlib import Path
 
-WITH_DEBUG = os.getenv('DEBUG') == '1'
+with_debug = os.getenv('DEBUG') == '1'
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+base_dir = Path(__file__).resolve().parent.parent
 
 # db
-DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-DATABASE_HOST = os.getenv('DATABASE_HOST')
-DATABASE_PORT = int(os.getenv('DATABASE_PORT'))
-DATABASE_NAME = os.getenv('DATABASE_NAME')
+database_username = os.getenv('DATABASE_USERNAME')
+database_password = os.getenv('DATABASE_PASSWORD')
+database_host = os.getenv('DATABASE_HOST')
+database_port = int(os.getenv('DATABASE_PORT'))
+database_name = os.getenv('DATABASE_NAME')
 
 # log
-LOG_DIR = BASE_DIR / 'logs'
-LOG_PATH = LOG_DIR / f'{datetime.now().strftime('%Y-%m-%d')}.log'
-
-# todoism
-TODOISM_ITEM_PER_PAGE = 20
-
-# flask
-SECRET_KEY = os.getenv('SECRET_KEY', 'YpL0hE3qZ8mT6vW2rK9cN1bF4sU7jX5')
+log_dir = base_dir / 'logs'
+log_path = log_dir / f'{datetime.now().strftime('%Y-%m-%d')}.log'
